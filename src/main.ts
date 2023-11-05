faf8fa4f1ccdabbdf88bc24fe04a2b66efb75ce7
Nabujjal Nath <nabujjal@gmail.com>
2023-11-05 23:06:20 +0530

    API key setup

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// Load environment variables from .env
+import 'dotenv/config';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
---
+++ src/utils/constant.ts
@@ -0,0 +1,4 @@
+const API_KEY = process.env["GOOGLE_API_KEY"];
+export const MOST_POPULAR_VIDEOS = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=[YOUR_API_KEY]`
+
+
