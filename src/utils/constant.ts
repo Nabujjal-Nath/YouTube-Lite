851665599536d808dce613d22aea010195142b4c
Nabujjal Nath <nabujjal@gmail.com>
2023-11-09 04:01:08 +0530

    display cards with flex-wrap

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,3 +1,3 @@
 // const API_KEY = process.env["GOOGLE_API_KEY"];
 import { environment } from "src/environments/environment"
-export const MOST_POPULAR_VIDEOS = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${environment.GOOGLE_API_KEY}`
+export const MOST_POPULAR_VIDEOS = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${environment.GOOGLE_API_KEY}`
