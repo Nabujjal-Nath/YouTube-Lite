31f2fcf1811a0de3005b5c86160fa48b3303474f
Nabujjal Nath <nabujjal@gmail.com>
2023-12-04 01:31:23 +0530

    video in video-player component added

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video-player',
   templateUrl: './video-player.component.html',
   styleUrls: ['./video-player.component.scss']
 })
-export class VideoPlayerComponent {
+export class VideoPlayerComponent implements OnInit {
+  videoID: string = ''
+  sanitizedVideoUrl!: SafeResourceUrl;
+  constructor(private route: ActivatedRoute,
+    private sanitizer: DomSanitizer) { }
 
+  ngOnInit(): void {
+    // Retrieve the id parameter from the route
+    this.route.queryParams.subscribe(params => {
+      this.videoID = params['id'];
+      this.sanitizedVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.videoID}?autoplay=1`);
+    });
+  }
 }
